2c91b4b77427a6b23e3baf31df3740a5e4852fd9
unknown <francleide7@gmail.com>
2015-06-05 20:43:45 -0300

    correcao da funcao busca_valor

--- teste.c
+++ teste.c
@@ -61,12 +61,12 @@ char *busca_pos(No **no, int x){
 int busca_valor(No **no, char x[]){
 	No *aux=*no;
 	int cont=0;
-	while(strcmp(aux->valor,x)!=0){
+	while(aux != NULL){
+		if(strcmp(aux->valor,x)==0)
+			return cont;
 		aux=aux->proximo;
 		cont++;
-	}if(strcmp(aux->valor,x)==0)
-	return cont;
-	else
+	}
 	return -1;
 }
 
@@ -94,10 +94,11 @@ escrever(&no,y,x);
 
 
 int main (int argc, char *argv[]) {
-    char *substituicao=argv[1], ch2[1], ch3[8], f[]="fifo", l[]="lru", r[]="random", k[]="R", w[]="W";
+    char *substituicao=argv[1], ch2[0], informacao[7], f[]="fifo", l[]="lru", r[]="random", k[]="R", w[]="W";
 	FILE *arquivo = fopen(argv[2],"r");
 	int t_pagina = atoi(argv[3]), t_memoria= atoi(argv[4]), pos=0;
-	int qtd_pagina=t_memoria/t_pagina, cont_page=0, cont_miss=0, cont_hit=0;
+	int qtd_pagina=t_memoria/t_pagina, cont_page=0, cont_miss=0, cont_hit=0, write_backs=0, op_leitura=0, op_escrita=0, w_fault = 0, op_entrada=0;
+//	int tx_pagefault=(100*w_fault)/op_escrita;
 	No *no= (No*) malloc(sizeof(No));
 	No *cabeca= NULL, *cauda=NULL;
 	
@@ -118,31 +119,53 @@ int main (int argc, char *argv[]) {
     }
     
     //Essa forma esta correta
-    while(fscanf(arquivo,"%s %s\n", ch3,ch2)!=EOF){   	
+    while(fscanf(arquivo,"%s %s\n",informacao,ch2)!=EOF){   
+	op_entrada++;	
         if(cont_page<qtd_pagina){//essa forma está correta! -- OK		
 	    	if(strcmp(ch2,k)==0){//esta correto
-				cont_miss++;//funciona
+				//funciona
+				int u;
+				if(cont_page==0){
+					strcpy(no->valor,informacao);
+    				no->proximo=NULL;
+					cabeca=no;
+					cauda=cabeca;			
+					op_leitura++;
+					cont_miss++;
+				}else
+				u= busca_valor(&cabeca,informacao);
+					if(u!=-1){
+						inserir(&cauda,informacao);
+						cont_miss++;
+						op_leitura++;
+						
+					}
+				
 			}else if(strcmp(ch2,w)==0){
 				if(cont_page==0){
-					strcpy(no->valor,ch3);
+					strcpy(no->valor,informacao);
     				no->proximo=NULL;
 					cabeca=no;
 					cauda=cabeca;		
-					printf("primeiro %s\n",cabeca->valor);		
+					printf("primeiro %s\n",cabeca->valor);	
+					op_escrita++;	
 				}else{
-						inserir(&cauda,ch3);
+					inserir(&cauda,informacao);
 					printf("nao eh primeiro %s\n",cauda->valor);
-				}
-				cont_page++;
+					op_escrita++;
+				}				
 			}
+			cont_page++;
 		}else
 		if(strcmp(substituicao,l)==0){//essa substituicao esta OK
 			if(strcmp(ch2,w)==0){
-				lru(no,ch3);
+				lru(no,informacao);
+				op_escrita++;
 			}
 			if(strcmp(ch2,k)==0){
 				int i;
-				i= busca_valor(&cabeca,ch3);
+				i= busca_valor(&cabeca,informacao);
+				op_leitura++;
 				if(i!=-1){
 					char x[8];
 					strcpy(x, busca_pos(&cabeca,i));
@@ -155,11 +178,11 @@ int main (int argc, char *argv[]) {
 		}
 		if(strcmp(substituicao,r)==0){//essa substituicao tem que TESTAR
 			if(strcmp(ch2,w)==0){
-				random(cabeca,ch3,cont_page);
+				random(cabeca,informacao,cont_page);
 			}
 			if(strcmp(ch2,k)==0){
 				int i;
-				i= busca_valor(&cabeca,ch3);
+				i= busca_valor(&cabeca,informacao);
 				if(i!=-1){
 					char x[8];
 					strcpy(x, busca_pos(&cabeca,i));
@@ -172,11 +195,11 @@ int main (int argc, char *argv[]) {
 		}
 		if(strcmp(substituicao,f)==0){//essa substituicao tem que TESTAR
 			if(strcmp(ch2,w)==0){
-				fifo(cabeca,ch3);
+				fifo(cabeca,informacao);
 			}
 			if(strcmp(ch2,k)==0){
 				int i;
-				i= busca_valor(&cabeca,ch3);
+				i= busca_valor(&cabeca,informacao);
 				if(i!=-1){
 					char x[8];
 					strcpy(x, busca_pos(&cabeca,i));
@@ -195,11 +218,16 @@ int main (int argc, char *argv[]) {
 	printf("o indice encontrado %i\n", busca_valor(&no,busca_pos(&no,1)));
 	remover(no,busca_pos(&no,1));
 	imprimir(cabeca);
+	
     printf("\nExecutando o simulador...\n");
     printf("Tamanho da memoria: %i KB\n", t_memoria);
     printf("Tamanho das paginas: %i KB\n", t_pagina);
     printf("Tecnica de reposicao: %s\n", substituicao);
-    printf("Hit: %i e Miss: %i\n", cont_hit, cont_miss);
+    printf("Operacoes no arquivo de entrada: %i\n", op_entrada);
+    printf("Operacoes de leitura: %i\nOperacoes de escrita: %i\n", cont_hit, cont_miss);
+    printf("Page Hits: %i \nPage Misses: %i\n", cont_hit, cont_miss);
+    printf("Numero de writebacks: %i\n", write_backs);
+//    printf("Taxa page fault: %i%\n", tx_pagefault);
 
     
     
