c21d7b5e37435c56c6a5d21782440e5577457060
unknown <francleide7@gmail.com>
2015-06-07 17:13:09 -0300

    correcao de erros e alteracoes das funcoes

--- teste.c
+++ teste.c
@@ -5,65 +5,82 @@
 
 //estrutura da lista (no)
 typedef struct No{
-    char valor[8];
+    char valor[9];
     struct No *proximo;
 }No;
+//variaveis globais
+char R[]="R", W[]="W";
+int cont_miss=0, cont_hit=0, write_backs=0, op_leitura=0, op_escrita=0, fault = 0, op_entrada=0;
+No *cabeca= NULL, *cauda=NULL, *no=NULL;
 
-//imprimir lista
-void imprimir(No *no){
-	No *aux=no;
+
+//imprimir lista --OK
+void imprimir(){
+	No *aux=cabeca;
     while (aux != NULL){
-        printf("%s\n", aux->valor);
+        printf("[%s] -> ", aux->valor);
         aux = aux->proximo;
     }
-   
+    printf("\n");
 }
 //inserir fim --OK
-void inserir(No **ant, char x[]){
+void inserir(char x[]){
 	No *novo =(No*) malloc(sizeof(No));
 	strcpy(novo->valor,x);
+	//x[8]='\0';
 	novo->proximo=NULL;
-	(*ant)->proximo=novo;
-	*ant=novo;
+	cauda->proximo=novo;
+	cauda=novo;
 }
-//escreve para a funcao random -- TESTAR
-void escrever(No **no,int pos, char x[]){
-	No *aux=*no;
+//escreve para a funcao random e quano for a mesma informacao -- TESTAR
+void escrever(int pos, char x[9]){
+	No *aux=cabeca;
 	int cont;
+	x[8]='\0';
 	for(cont=0;cont<pos;cont++){
 		aux->proximo;
 		aux=aux->proximo;
 	}	
 	strcpy(aux->valor,x);
 }
-//remove para todas as funcoes -- OK
-void remover(No *no, char x[]){
-	No *anterior, *atual=no;
-	int cont, y = busca_valor(&no,x);
-	for (cont=0; cont<y;cont++){
-		anterior=atual;
+//remove inicio-- OK
+void remover(){
+	printf("chamou funcao remover\n" );
+	if(cabeca==NULL){
+		printf("lista vazia");
+		return;
+	}
+	cabeca=cabeca->proximo;
+}
+//trocar o valor encontrado para tornalo recente
+void trocar(char x[]){
+	No *atual=cabeca, *ant=NULL;
+	while(atual!=NULL){
+		if(strcmp(atual->valor,x)==0){
+			if(ant==NULL){
+				remover();
+			}else{
+				if(atual->proximo!=NULL){
+					ant->proximo=atual->proximo;
+				}else{
+					cauda=ant;
+				}		
+			}
+			inserir(x);	
+		}
+		ant=atual;
 		atual=atual->proximo;
 	}
-	anterior->proximo=atual->proximo;
-	
-}
-//busca o valor pela posicao e retorna valor encontrado --OK
-char *busca_pos(No **no, int x){
-	No *aux=*no;
-	int cont=0;
-	while(cont<x){
-		aux=aux->proximo;
-		cont++;
-	}
-	return aux->valor;
+	printf("chamou funcao troca\n" );
 }
 //busca a posicao pelo valor e retorna valor encontrado --OK
-int busca_valor(No **no, char x[]){
-	No *aux=*no;
+int busca_valor(char x[]){
+	No *aux=cabeca;
 	int cont=0;
 	while(aux != NULL){
-		if(strcmp(aux->valor,x)==0)
+		if(strcmp(aux->valor,x)==0){
 			return cont;
+		}
 		aux=aux->proximo;
 		cont++;
 	}
@@ -71,36 +88,100 @@ int busca_valor(No **no, char x[]){
 }
 
 
+//funcao de substituicao fifo
+void s_fifo(char informacao[],char wr[]){
+imprimir();
+int i= busca_valor(informacao);	
+	if(strcmp(wr,W)==0){
+		if(i==-1){
+				remover();
+				inserir(informacao);	
+		}else {
+			trocar(informacao);
+		}
+		op_escrita++;
+	}
+	if(strcmp(wr,R)==0){
+		if(i==-1){	
+			remover();
+			inserir(informacao);
+			op_escrita++;
+			cont_miss++;
+		}else{
+			cont_hit++;
+		}
+		op_leitura++;
+		}
+		fault++;
+		write_backs++;
+}
+//funcao de substituicao lru
+void s_lru(char informacao[],char wr[]){
+	imprimir();
+	int i= busca_valor(informacao);	
+	if(strcmp(wr,W)==0){
+		if(i==-1){
+				remover();
+				inserir(informacao);
+		}else {
+			trocar(informacao);
+		}
+		op_escrita++;
+	}
+	if(strcmp(wr,R)==0){
+		if(i==-1){
+			remover();
+			inserir(informacao);
+			op_escrita++;
+			cont_miss++;
+		
+		}else{
+			cont_hit++;
+			trocar(informacao);
+		}
+		op_leitura++;
+		}
+		fault++;
+		write_backs++;
+}
+//funcao de substituicao random
+void s_random(char informacao[],char wr[],int cont_page){
+	imprimir();
+	int n;
+	//srand(time(NULL));
+	n=(rand()%cont_page);
+	printf("rand %i\n",n);
+	int i= busca_valor(informacao);	
+	if(strcmp(wr,W)==0){
+		if(i==-1){
+			escrever(n,informacao);	
+		}else {
+			escrever(i,informacao);	
+		}
+		op_escrita++;
+	}
+	if(strcmp(wr,R)==0){
+		if(i==-1){
+			escrever(n,informacao);	
+			cont_miss++;			
+		}else{
+			cont_hit++;
+		}
+		op_leitura++;
+		}
+		fault++;
+		write_backs++;
+}
 
-void fifo(No *no, char x[]){
-remover(no,busca_pos(&no,0));
-inserir(&no,x);	
-}
-void lru(No *no, char x[]){
-remover(no,busca_pos(&no,0));
-inserir(&no,x);		
-/*pode ser feita com comparacao utilizando a busca até q seja 
-igual p/ poder ser removido o valor atualizado	
-*/
-}
-void random(No *no, char x[],int i){
-int n,y;
-srand((unsigned)time(NULL));
-n=(rand()%i);
-//escrever na posicao
-y=busca_valor(&no,busca_pos(&no,n));
-escrever(&no,y,x);	
-}
 
 
 int main (int argc, char *argv[]) {
-    char *substituicao=argv[1], ch2[0], informacao[7], f[]="fifo", l[]="lru", r[]="random", k[]="R", w[]="W";
+    char *substituicao=argv[1], ch2[1], informacao[9], fifo[]="fifo", lru[]="lru", random[]="random";
 	FILE *arquivo = fopen(argv[2],"r");
 	int t_pagina = atoi(argv[3]), t_memoria= atoi(argv[4]), pos=0;
-	int qtd_pagina=t_memoria/t_pagina, cont_page=0, cont_miss=0, cont_hit=0, write_backs=0, op_leitura=0, op_escrita=0, w_fault = 0, op_entrada=0;
-//	int tx_pagefault=(100*w_fault)/op_escrita;
-	No *no= (No*) malloc(sizeof(No));
-	No *cabeca= NULL, *cauda=NULL;
+	int qtd_pagina=t_memoria/t_pagina, cont_page=0, i;
+	unsigned int tx_pagefault;
+	no= (No*) malloc(sizeof(No));
 	
     //retorna um erro se o tamanho da paginacao estiver fora do intervalo permitido
 	if(t_pagina<2 || t_pagina>64){
@@ -118,121 +199,93 @@ int main (int argc, char *argv[]) {
 		return 0;
     }
     
-    //Essa forma esta correta
-    while(fscanf(arquivo,"%s %s\n",informacao,ch2)!=EOF){   
-	op_entrada++;	
-        if(cont_page<qtd_pagina){//essa forma está correta! -- OK		
-	    	if(strcmp(ch2,k)==0){//esta correto
-				//funciona
+    //essa forma está correta! -- OK
+	while(fscanf(arquivo,"%s %s\n",informacao,ch2)!=EOF){   
+        if(cont_page<qtd_pagina){		
+    		if(strcmp(ch2,R)==0){
 				int u;
 				if(cont_page==0){
 					strcpy(no->valor,informacao);
     				no->proximo=NULL;
 					cabeca=no;
 					cauda=cabeca;			
-					op_leitura++;
 					cont_miss++;
-				}else
-				u= busca_valor(&cabeca,informacao);
-					if(u!=-1){
-						inserir(&cauda,informacao);
-						cont_miss++;
-						op_leitura++;
-						
-					}
-				
-			}else if(strcmp(ch2,w)==0){
-				if(cont_page==0){
-					strcpy(no->valor,informacao);
-    				no->proximo=NULL;
-					cabeca=no;
-					cauda=cabeca;		
-					printf("primeiro %s\n",cabeca->valor);	
-					op_escrita++;	
-				}else{
-					inserir(&cauda,informacao);
-					printf("nao eh primeiro %s\n",cauda->valor);
 					op_escrita++;
-				}				
-			}
-			cont_page++;
-		}else
-		if(strcmp(substituicao,l)==0){//essa substituicao esta OK
-			if(strcmp(ch2,w)==0){
-				lru(no,informacao);
-				op_escrita++;
-			}
-			if(strcmp(ch2,k)==0){
-				int i;
-				i= busca_valor(&cabeca,informacao);
+				}else{
+					u = busca_valor(informacao);
+					if(u==-1){
+						inserir(informacao);
+						op_escrita++;
+						cont_miss++;
+					}else{
+						if(strcmp(substituicao,lru)==0){
+							trocar(informacao);
+						}else
+						escrever(u,informacao);
+						cont_hit++;			
+					}	
+				}
 				op_leitura++;
-				if(i!=-1){
-					char x[8];
-					strcpy(x, busca_pos(&cabeca,i));
-					remover(cabeca,x);
-					lru(cabeca,x);
-					cont_hit++;
-				}else
-				cont_miss++;
+			}
+			else {
+				if(strcmp(ch2,W)==0){
+					if(cont_page==0){
+						strcpy(no->valor,informacao);
+	    				no->proximo=NULL;
+						cabeca=no;
+						cauda=cabeca;			
+					}else {
+						int u=busca_valor(informacao);
+						if(u==-1){
+							inserir(informacao);
+						}
+						else{
+							if(strcmp(substituicao,lru)==0){
+								trocar(informacao);
+							}else
+							escrever(u,informacao);
+						}		
+					}
+					op_escrita++;			
+				}	
+			}	
+			cont_page++;
+		}
+		else{
+			if(strcmp(substituicao,lru)==0){//essa substituicao esta OK
+				s_lru(informacao,ch2);
+			}
+			if(strcmp(substituicao,random)==0){//essa substituicao esta OK
+				s_random(informacao,ch2,cont_page);
+			}
+			if(strcmp(substituicao,fifo)==0){//essa substituicao esta OK
+				s_fifo(informacao,ch2);
 			}
 		}
-		if(strcmp(substituicao,r)==0){//essa substituicao tem que TESTAR
-			if(strcmp(ch2,w)==0){
-				random(cabeca,informacao,cont_page);
-			}
-			if(strcmp(ch2,k)==0){
-				int i;
-				i= busca_valor(&cabeca,informacao);
-				if(i!=-1){
-					char x[8];
-					strcpy(x, busca_pos(&cabeca,i));
-					remover(cabeca,x);
-					random(cabeca,x,cont_page);
-					cont_hit++;
-				}else
-				cont_miss++;
-			}
-		}
-		if(strcmp(substituicao,f)==0){//essa substituicao tem que TESTAR
-			if(strcmp(ch2,w)==0){
-				fifo(cabeca,informacao);
-			}
-			if(strcmp(ch2,k)==0){
-				int i;
-				i= busca_valor(&cabeca,informacao);
-				if(i!=-1){
-					char x[8];
-					strcpy(x, busca_pos(&cabeca,i));
-					remover(cabeca,x);
-					fifo(cabeca,x);
-					cont_hit++;
-				}else
-				cont_miss++;	
-			}
-		}
+		op_entrada++;
 	}
-	
-	
-	imprimir(cabeca);
-	printf("o indice encontrado %s\n", busca_pos(&no,1));
-	printf("o indice encontrado %i\n", busca_valor(&no,busca_pos(&no,1)));
-	remover(no,busca_pos(&no,1));
-	imprimir(cabeca);
-	
+	tx_pagefault=(100*fault)/op_leitura;
+	//imprime a lista
+	imprimir();
+	//pequeno relatorio
     printf("\nExecutando o simulador...\n");
     printf("Tamanho da memoria: %i KB\n", t_memoria);
     printf("Tamanho das paginas: %i KB\n", t_pagina);
     printf("Tecnica de reposicao: %s\n", substituicao);
     printf("Operacoes no arquivo de entrada: %i\n", op_entrada);
-    printf("Operacoes de leitura: %i\nOperacoes de escrita: %i\n", cont_hit, cont_miss);
+    printf("Operacoes de leitura: %i\nOperacoes de escrita: %i\n", op_leitura, op_escrita);
     printf("Page Hits: %i \nPage Misses: %i\n", cont_hit, cont_miss);
     printf("Numero de writebacks: %i\n", write_backs);
-//    printf("Taxa page fault: %i%\n", tx_pagefault);
+	printf("Taxa page fault: %i%%\n", tx_pagefault);
 
     
     
     fclose(arquivo);
+    
+   /*free(cabeca);
+    free(cauda);*/
     free(no);
+    
     //system("pause");
 return 0;
 }
