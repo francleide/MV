b9b4e466906569184717bad944ea896e217a47d1
unknown <francleide7@gmail.com>
2015-06-14 15:52:21 -0300

    alteracao final

--- main.c
+++ main.c
@@ -8,9 +8,9 @@ typedef struct No{
     char valor[9];
     struct No *proximo;
 }No;
+
 //variaveis globais
-char R[]="R", W[]="W";
-int cont_miss=0, cont_hit=0, write_backs=0, op_leitura=0, op_escrita=0, fault = 0, op_entrada=0;
+int i;
 No *cabeca= NULL, *cauda=NULL, *no=NULL;
 
 
@@ -27,7 +27,6 @@ void imprimir(){
 void inserir(char x[]){
 	No *novo =(No*) malloc(sizeof(No));
 	strcpy(novo->valor,x);
-	//x[8]='\0';
 	novo->proximo=NULL;
 	cauda->proximo=novo;
 	cauda=novo;
@@ -52,8 +51,8 @@ void remover(){
 	}
 	cabeca=cabeca->proximo;
 }
-//trocar o valor encontrado para torna-lo recente
-void trocar(char x[]){
+//deslocar o valor encontrado para torna-lo recente
+void deslocar(char x[]){
 	No *atual=cabeca, *ant=NULL;
 	while(atual!=NULL){
 		if(strcmp(atual->valor,x)==0){
@@ -89,104 +88,55 @@ int busca_valor(char x[]){
 
 
 //funcao de substituicao fifo
-void s_fifo(char informacao[],char wr[]){
+void s_fifo(char informacao[]){
 imprimir();
 int i= busca_valor(informacao);	
-	if(strcmp(wr,W)==0){
 		if(i==-1){
 			remover();
 			inserir(informacao);	
 		}else {
 			escrever(i,informacao);
 		}
-		op_escrita++;
-	}
-	if(strcmp(wr,R)==0){
-		if(i==-1){	
-			remover();
-			inserir(informacao);
-			op_escrita++;
-			cont_miss++;
-			fault++;
-		}else{
-			cont_hit++;
-		}
-		op_leitura++;
-		}
-		
-		write_backs++;
 }
 //funcao de substituicao lru
-void s_lru(char informacao[],char wr[]){
+void s_lru(char informacao[]){
 	imprimir();
 	int i= busca_valor(informacao);	
-	if(strcmp(wr,W)==0){
 		if(i==-1){
 			remover();
 			inserir(informacao);
 		}else {
-			trocar(informacao);
-		}
-		op_escrita++;
-	}
-	if(strcmp(wr,R)==0){
-		if(i==-1){
-			remover();
-			inserir(informacao);
-			op_escrita++;
-			cont_miss++;
-			fault++;
-		}else{
-			cont_hit++;
-			trocar(informacao);
-		}
-		op_leitura++;
+			deslocar(informacao);
 		
 		}
-		
-		write_backs++;
 }
 //funcao de substituicao random
-void s_random(char informacao[],char wr[],int cont_page){
+void s_random(char informacao[],int cont_page){
 	imprimir();
 	int n;
 	//srand(time(NULL));
 	n=(rand()%cont_page);
 	printf("rand %i\n",n);
 	int i= busca_valor(informacao);	
-	if(strcmp(wr,W)==0){
 		if(i==-1){
 			escrever(n,informacao);	
 		}else {
 			escrever(i,informacao);	
 		}
-		op_escrita++;
-	}
-	if(strcmp(wr,R)==0){
-		if(i==-1){
-			escrever(n,informacao);	
-			cont_miss++;
-			fault++;			
-		}else{
-			cont_hit++;
-		}
-		op_leitura++;
-		}
-		
-		write_backs++;
 }
 
 
 
 int main (int argc, char *argv[]) {
-    char *substituicao=argv[1], ch2[1], informacao[9], fifo[]="fifo", lru[]="lru", random[]="random";
+    char *substituicao=argv[1], ch2[2], informacao[9], fifo[]="fifo", lru[]="lru", random[]="random", R[]="R", W[]="W";
 	FILE *arquivo = fopen(argv[2],"r");
-	int t_pagina = atoi(argv[3]), t_memoria= atoi(argv[4]), pos=0;
-	int qtd_pagina=t_memoria/t_pagina, cont_page=0, i;
+	int t_pagina = atoi(argv[3]), t_memoria= atoi(argv[4]), pos=0,  cont_miss=0, cont_hit=0, write_back=0, cont_op_leitura=0, cont_op_escrita=0, cont_op_entrada=0;
+	int qtd_pagina=t_memoria/t_pagina, cont_page=0, cont_fault = 0;
 	clock_t inicio,fim;
 	unsigned int tx_pagefault;
 	no= (No*) malloc(sizeof(No));
-	inicio=clock();
+	srand(time(NULL));
+	
     //retorna um erro se o tamanho da paginacao estiver fora do intervalo permitido
 	if(t_pagina<2 || t_pagina>64){
 		printf("Erro! Esse tamanho de paginacao nao e possivel\n");
@@ -202,34 +152,31 @@ int main (int argc, char *argv[]) {
            printf("Erro! Nao e possivel abrir o arquivo\n");
 		return 0;
     }
-    
-    //essa forma esta correta! -- OK
+    inicio=clock();
+    //essa forma está correta! -- OK
 	while(fscanf(arquivo,"%s %s\n",informacao,ch2)!=EOF){   
         if(cont_page<qtd_pagina){		
     		if(strcmp(ch2,R)==0){
-				int u;
+				
 				if(cont_page==0){
 					strcpy(no->valor,informacao);
     				no->proximo=NULL;
 					cabeca=no;
-					cauda=cabeca;			
+					cauda=cabeca;	
 					cont_miss++;
-					op_escrita++;
 				}else{
-					u = busca_valor(informacao);
-					if(u==-1){
+					i = busca_valor(informacao);
+					if(i==-1){
 						inserir(informacao);
-						op_escrita++;
 						cont_miss++;
 					}else{
 						if(strcmp(substituicao,lru)==0){
-							trocar(informacao);
-						}else
-						escrever(u,informacao);
+							deslocar(informacao);
+						}
 						cont_hit++;			
 					}	
 				}
-				op_leitura++;
+				cont_op_leitura++;
 			}
 			else {
 				if(strcmp(ch2,W)==0){
@@ -239,51 +186,66 @@ int main (int argc, char *argv[]) {
 						cabeca=no;
 						cauda=cabeca;			
 					}else {
-						int u=busca_valor(informacao);
-						if(u==-1){
+						int i=busca_valor(informacao);
+						if(i==-1){
 							inserir(informacao);
 						}
 						else{
 							if(strcmp(substituicao,lru)==0){
-								trocar(informacao);
-							}else
-							escrever(u,informacao);
+								deslocar(informacao);
+							}
 						}		
 					}
-					op_escrita++;			
+					cont_op_escrita++;			
 				}	
 			}	
 			cont_page++;
 		}
 		else{
-			if(strcmp(substituicao,lru)==0){//essa substituicao esta OK
-				s_lru(informacao,ch2);
+				
+				if(strcmp(substituicao,lru)==0){//essa substituicao esta OK
+					s_lru(informacao);
+				}
+				if(strcmp(substituicao,random)==0){//essa substituicao esta OK
+					s_random(informacao,cont_page);
+				}
+				if(strcmp(substituicao,fifo)==0){//essa substituicao esta OK
+					s_fifo(informacao);
+				}
+			if(strcmp(ch2,W)==0){
+				cont_op_escrita++;
+				write_back++;
+				cont_fault++;			
 			}
-			if(strcmp(substituicao,random)==0){//essa substituicao esta OK
-				s_random(informacao,ch2,cont_page);
-			}
-			if(strcmp(substituicao,fifo)==0){//essa substituicao esta OK
-				s_fifo(informacao,ch2);
+			else if(strcmp(ch2,R)==0){
+				i=busca_valor(informacao);
+				if(i==-1){
+					cont_miss++;
+				}else
+					cont_hit++;	
+								
+				cont_op_leitura++;
 			}
 		}
-		op_entrada++;
+		cont_op_entrada++;
 	}
-	tx_pagefault=(100*fault)/op_leitura;
+	tx_pagefault=(100*cont_fault)/cont_op_escrita;
 	//imprime a lista
 	imprimir();
 	//pequeno relatorio
+	fim=clock();
+	printf("\nTempo %f ms\n\n",(fim-inicio)/(float)CLOCKS_PER_SEC*1000);
     printf("\nExecutando o simulador...\n");
     printf("Tamanho da memoria: %i KB\n", t_memoria);
     printf("Tamanho das paginas: %i KB\n", t_pagina);
     printf("Tecnica de reposicao: %s\n", substituicao);
     printf("Numero de paginas: %i\n", qtd_pagina);
-    printf("Operacoes no arquivo de entrada: %i\n", op_entrada);
-    printf("Operacoes de leitura: %i\nOperacoes de escrita: %i\n", op_leitura, op_escrita);
+    printf("Operacoes no arquivo de entrada: %i\n", cont_op_entrada);
+    printf("Operacoes de leitura: %i\nOperacoes de escrita: %i\n", cont_op_leitura, cont_op_escrita);
     printf("Page Hits: %i \nPage Misses: %i\n", cont_hit, cont_miss);
-    printf("Numero de writebacks: %i\n", write_backs);
+    printf("Numero de writebacks: %i\n", write_back);
 	printf("Taxa page fault: %i%%\n", tx_pagefault);
-	fim=clock();
-	printf("Tempo %f ms\n",(fim-inicio)/(float)CLOCKS_PER_SEC*1000);
+	
 
     
     
@@ -297,3 +259,4 @@ int main (int argc, char *argv[]) {
     //system("pause");
 return 0;
 }
+
